0f3ab08807f0696d16e12be4f9725ff74865fe85
Kyrylo Khlopko <kkhlopko@readdle.com>
2025-02-28 13:00:53 +0100

    Cleanup: remove fuzzy detection

--- Sources/RDCld2/LanguageDetector.swift
+++ Sources/RDCld2/LanguageDetector.swift
@@ -24,25 +24,6 @@ public class LanguageDetector {
         self.detectLanguage(string: string, strict: strict, isPlainText: false)?.languages ?? []
     }
 
-    public class func fuzzyDetectLanguage(string: String, strict: Bool = false, isPlainText: Bool = false) -> String? {
-        guard let result = self.detectLanguage(string: string, strict: strict, isPlainText: isPlainText) else {
-            return nil
-        }
-
-        if result.languages.count >= 2, result.percents.count >= 2 {
-            let lang1 = result.languages[0]
-            let lang2 = result.languages[1]
-            let lang2Score = result.percents[1]
-
-            if lang1 == "en",
-               self.isLatinBased(languageCode: lang2),
-               lang2Score >= 50 {
-                return lang2
-            }
-        }
-        return result.languages.first
-    }
-
     public class func detectLanguage(string: String, strict: Bool = false, isPlainText: Bool = false) -> LanguageDetectionResult? {
         guard let data = string.data(using: .utf8) else {
             assert(false)
@@ -75,63 +56,4 @@ public class LanguageDetector {
             return LanguageDetectionResult(languages: languages, percents: percents, isReliable: cResult.is_reliable > 0)
         }
     }
-    
-    private class func isLatinBased(languageCode: String) -> Bool {
-        return self.latinBasedLanguages.contains(languageCode)
-    }
-
-    private static let latinBasedLanguages: Set<String> = [
-        "af", // Afrikaans
-        "ast", // Asturian
-        "eu", // Basque
-        "br", // Breton
-        "ca", // Catalan
-        "ceb", // Cebuano
-        "ch", // Chamorro
-        "kw", // Cornish
-        "co", // Corsican
-        "cs", // Czech
-        "da", // Danish
-        "nl", // Dutch
-        "en", // English
-        "eo", // Esperanto
-        "et", // Estonian
-        "fo", // Faroese
-        "fi", // Finnish
-        "fr", // French
-        "fy", // Frisian
-        "gl", // Galician
-        "de", // German
-        "kl", // Greenlandic
-        "hu", // Hungarian
-        "is", // Icelandic
-        "id", // Indonesian
-        "ga", // Irish
-        "it", // Italian
-        "ku", // Kurdish (Latin)
-        "la", // Latin
-        "lv", // Latvian
-        "lt", // Lithuanian
-        "lb", // Luxembourgish
-        "ms", // Malay
-        "mt", // Maltese
-        "gv", // Manx
-        "no", // Norwegian
-        "oc", // Occitan
-        "pl", // Polish
-        "pt", // Portuguese
-        "ro", // Romanian
-        "sco", // Scots
-        "gd", // Scottish Gaelic
-        "sk", // Slovak
-        "sl", // Slovenian
-        "es", // Spanish
-        "sw", // Swahili
-        "sv", // Swedish
-        "tl", // Tagalog
-        "tr", // Turkish
-        "vi", // Vietnamese
-        "cy", // Welsh
-        "wa", // Walloon
-    ]
 }
